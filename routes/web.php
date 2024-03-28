6ef52d0c356d70720fd1ac433cb38ab8293f1249
dev2 <ko5642027@naver.com>
2024-03-28 17:47:21 +0900

    회원가입 폼 추가

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\View\RegisterViewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [RegisterViewController::class, 'showRegistrationForm'])->name('view.register');
+Route::post('/api/register', [RegisterController::class, 'register']);
