3fbbc5b1b3c453c32cf916cfb5bf6ca18a8d6137
dev2 <ko5642027@naver.com>
2024-04-15 10:53:35 +0900

    tradingview api push

--- resources/views/exchange/left.blade.php
+++ resources/views/exchange/left.blade.php
@@ -3,20 +3,19 @@
     <div id="chartContainer"></div>
 </div>
 
-<script src="{{ asset('/charting_library/charting_library.standalone.js') }}"></script>
+<script src="{{ asset('/charting_library/charting_library.js') }}"></script>
 <script src="{{ asset('/datafeeds/udf/dist/bundle.js') }}"></script>
 
 <script>
     document.addEventListener('livewire:init', () => {
-        let tvWidget = null; // TradingView 차트 위젯 변수 선언
+        var widget = null; // TradingView 차트 위젯 변수 선언
 
         Livewire.on('initializeChart', (symbol) => {
-            // 새로운 symbol이 주어질 때마다 차트를 업데이트
-
-            if (tvWidget !== null) {
-                tvWidget.chart().setSymbol(symbol[0], '1D', () => {
-                    console.log('Chart updated with symbol:', symbol[0]);
-                });
+            if (widget !== null) {
+                widget.remove();
+                // widget.chart().setSymbol(symbol[0], '1D', () => {
+                //     console.log('Chart updated with symbol:', symbol[0]);
+                // });
             } else {
                 // 처음으로 차트를 생성할 때
                 initializeChart(symbol[0]);
@@ -34,7 +33,7 @@
                 fullscreen: true
             };
 
-            tvWidget = new TradingView.widget(widgetOptions);
+            widget = new TradingView.widget(widgetOptions);
         }
     });
 </script>
